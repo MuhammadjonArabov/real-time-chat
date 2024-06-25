45e059a385d3c7763f0d3672b8103d5a4e455b14
MuhammadjonArabov <arabovmuhammadjon1301@gmail.com>
2024-06-25 17:50:54 +0500

    Add gitignore files

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ import environ
 
 
 env = environ.Env()
-environ.Env.read_env(os.path.join(os.path.dirname(__file__), '.env'))
+environ.Env.read_env(os.path.join(os.path.dirname(__file__), '../.env'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
